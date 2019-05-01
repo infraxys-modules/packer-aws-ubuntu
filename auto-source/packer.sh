d9d3164443fc4f42264e44f94e6ac125b257d46b
jeroen-manders <jeroen.manders@external.mckinsey.com>
2019-05-01 11:37:41 +0200

    debug

--- auto-source/packer.sh
+++ auto-source/packer.sh
@@ -1,8 +1,1 @@
 export packer_directory="$(pwd)/packer";
-
-echo "packer_directory: $packer_directory";
-echo "packer_directory: $packer_directory";
-echo "packer_directory: $packer_directory";
-echo "packer_directory: $packer_directory";
-
-ls -ltr $packer_directory
